e92472ec04147873b4fdb2a1b38e126f2b2fcad4
JavaScythe <slashmod6@gmail.com>
2022-11-16 19:18:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
+var proxy = require('express-http-proxy');
 var bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/collect', proxy('https://hardwareSurvery.i10.repl.co/collect'));
 const requestIp = require('request-ip');
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 app.get("/what", function(req, res) {
 	res.send(['CHROME VERSION', 'CHROMEOS_FIRMWARE_VERSION', 'CHROMEOS_BOARD_APPID', 'GOOGLE_RELEASE', 'CHROMEOS_RELEASE_DESCRIPTION', 'CHROMEOS_RELEASE_BOARD', 'HWID', 'LOGDATE', 'ONBOARDING_TIME', 'account_type', 'ifconfig', 'monitor_info', 'uname', 'platform_identity_name', 'platform_identity_model', 'ENTERPRISE_ENROLLED', 'network_devices', 'network_services', 'power_supply_info', 'os-release BUILD_ID', 'os-release GOOGLE_CRASH_ID', 'oemdata', 'meminfo', 'lsusb'].join("<br>"));
-});
-app.post("/collect", (req, res) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	let collectionId = uuidv4();
-	//console.log(req.body);
-	res.send("<pre>Survey Complete</pre><br><pre>DM " + collectionId + " to [object Object]#3827 to claim your 10000 coin reward</pre>");
-	let d = JSON.parse(fs.readFileSync(__dirname + "/data.json"));
-	req.body["remote_ip"] = requestIp.getClientIp(req);
-	d[collectionId] = req.body;
-	fs.writeFileSync(__dirname + "/data.json", JSON.stringify(d));
 });
 app.get("/count", (req, res) => {
 	let d = JSON.parse(fs.readFileSync(__dirname + "/data.json"));
